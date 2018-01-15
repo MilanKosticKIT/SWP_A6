fd5bc368f03fa704298c6e4f3471523b5c5fc450
Milan_K <allgemein@milan-kostic.de>
2018-01-15 15:27:25 +0100

    Initial Commit

---
+++ Aufgabe_6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Aufgabe_6
+//
+//  Created by Milan Kostic on 15.01.18.
+//  Copyright © 2018 Milan Kostic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
